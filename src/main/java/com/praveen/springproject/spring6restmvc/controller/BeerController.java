3dc904643292079a02046b329eff88ddb4403080
praveenbabu36 <praveen.babu36@gmail.com>
2023-11-18 12:15:38 -0500

    Logging using Lombok

--- src/main/java/com/praveen/springproject/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/praveen/springproject/spring6restmvc/controller/BeerController.java
@@ -3,18 +3,25 @@ package com.praveen.springproject.spring6restmvc.controller;
 import com.praveen.springproject.spring6restmvc.model.Beer;
 import com.praveen.springproject.spring6restmvc.service.BeerService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 
 import java.util.ArrayList;
 import java.util.UUID;
-
+@Slf4j
 @AllArgsConstructor
 @Controller
 public class BeerController {
 
     private final BeerService beerService;
 
+    public Beer getBeerById(UUID id){
+        log.debug("In BeerController. Get Beer by Id.");
+        return beerService.getBeerById(id);
+    }
     public Beer getMyFavBeer() {
+
+        log.debug("In BeerController. Get my Fav. Beer.");
         return beerService.getBeerById(UUID.randomUUID());
     }
 }
--- src/main/java/com/praveen/springproject/spring6restmvc/service/BeerServiceImpl.java
+++ src/main/java/com/praveen/springproject/spring6restmvc/service/BeerServiceImpl.java
@@ -2,14 +2,19 @@ package com.praveen.springproject.spring6restmvc.service;
 
 import com.praveen.springproject.spring6restmvc.model.Beer;
 import com.praveen.springproject.spring6restmvc.model.BeerStyle;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
+@Slf4j
+@Service
 public class BeerServiceImpl implements BeerService {
     @Override
     public Beer getBeerById(UUID id) {
 
+        log.debug("In BeerServiceImpl. Get Beer by ID " + id);
         return Beer.builder()
                 .id(id)
                 .name("India Pale Ale")
